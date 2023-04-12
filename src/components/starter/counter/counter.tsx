07150d83a9e5b6814ae77bffe9e9fd085f302fc7
ThomasDeutsch <ThomasDeutsch@users.noreply.github.com>
2023-04-12 20:55:55 +0200

    3 files modified

--- src/components/starter/counter/counter.tsx
+++ src/components/starter/counter/counter.tsx
@@ -3,61 +3,69 @@ import {
   noSerialize,
   useComputed$,
   useSignal,
+  $,
   useStylesScoped$,
   useVisibleTask$,
+  useTask$,
 } from '@builder.io/qwik';
 import styles from './counter.css?inline';
-import { Scheduler, request, Event, askFor } from '@flowcards/core';
+import {
+  Scheduler,
+  request,
+  Event,
+  askFor,
+  EventUpdateInfo,
+} from '@flowcards/core';
 
 // THE EVENT OBJECT HOLDS INFORMATION ABOUT A SPECIFIC EVENT, LIKE VALUE, OR ISPENDING
 export const countEvent = new Event<number>('count');
+const singalInfo = {
+  registerCallback: $(countEvent.registerCallback.bind(countEvent)),
+  set: $(countEvent.set.bind(countEvent)),
+  validate: $(countEvent.validate.bind(countEvent)),
+  isValid: $(countEvent.isValid.bind(countEvent)),
+};
 
 // THIS IS A COUNTER IN SCENARIO BASED PROGRAMMING:
 export const scheduler = new Scheduler({
   rootFlow: function* () {
     yield request(countEvent, 100); // INITIAL EVENT VALUE
     while (true) {
-      yield askFor(countEvent); // ASK THE USER TO SUBMIT A NEW COUNT VALUE
+      yield askFor(countEvent, (next) => next < 106); // ASK THE USER TO SUBMIT A NEW COUNT VALUE
       console.log('askFor count', countEvent.value);
     }
   },
 });
 
-export function useFCSignal<T>(event: Event<T>) {
-  const signal = useSignal<T | undefined>(event.value);
+export function useFCSignal<T>(signalInfo: any) {
+  const signal = useSignal<T | undefined>(signalInfo.value);
   useVisibleTask$(() => {
-    event.registerCallback(() => {
-      signal.value = event.value;
-    });
+    console.log('register!');
+    signalInfo.registerCallback((info: EventUpdateInfo<T>) => {
+      signal.value = info.value;
+    }, true);
   });
-  return useComputed$(() => signal.value);
+  return useComputed$(() => {
+    return signal.value;
+  });
 }
 
 export default component$(() => {
   useStylesScoped$(styles);
-  // ----- start: i would like to export this into a custom hook.
-  //              see the useFCSignal hook.
-  const count = useSignal(countEvent.value);
-  useVisibleTask$(() => {
-    console.log(
-      'callback: everytime the event-value chanes, update the signal value'
-    );
-    countEvent.registerCallback(() => {
-      console.log('updating signal value to: ', countEvent.value);
-      count.value = countEvent.value;
-    });
-  });
-  // ------ end
-  // WILL THROW AN ERROR:
-  //const count2 = useFCSignal(countEvent);
+  const count = useFCSignal<number>(singalInfo);
+  const isValid = useComputed$(() =>
+    singalInfo.isValid((count.value || 0) + 1)
+  );
 
   return (
     <div class="counter-wrapper">
-      <button onClick$={() => countEvent.set((count.value || 0) - 1)}>-</button>
+      <button onClick$={() => singalInfo.set((count.value || 0) - 1)}>-</button>
       <span class={`counter-value ${count.value || 0 % 2 === 0 ? 'odd' : ''}`}>
         {count.value}
       </span>
-      <button onClick$={() => countEvent.set((count.value || 0) + 1)}>+</button>
+
+      <button onClick$={() => singalInfo.set((count.value || 0) + 1)}>+</button>
+      {isValid.value ? 'valid' : 'invalid'}
     </div>
   );
 });
